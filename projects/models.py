ea418791982284d4ba38491ab7655ffe79d518e9
ChadSutherland <csuth1@live.com>
2019-11-27 23:02:00 -0500

    Add fields for link and coordinator to Project model

--- projects/models.py
+++ projects/models.py
@@ -5,4 +5,6 @@ class Project(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
 	technology = models.CharField(max_length=20)
+	coordinator = models.CharField(max_length=70)
+	link = models.CharField(max_length=100)
 	image = models.FilePathField(path="/img")
