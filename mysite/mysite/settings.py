d75bb3891b98f2c058b2efa25aabb56aaa1f75a3
ChadSutherland <csuth1@live.com>
2020-01-18 10:08:07 -0500

    Upgraded django due to security vulnerability

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Install PyMySQL as mysqlclient/MySQLdb to use Django's mysqlclient adapter
 # See https://docs.djangoproject.com/en/2.1/ref/databases/#mysql-db-api-drivers
 # for more information
-import pymysql  # noqa: 402
-pymysql.install_as_MySQLdb()
+# import pymysql  # noqa: 402
+# pymysql.install_as_MySQLdb()
 
 # [START db_setup]
 if os.getenv('GAE_APPLICATION', None):
