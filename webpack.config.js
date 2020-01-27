c075826ff340b69245b959233cedfef679050341
ChadSutherland <csuth1@live.com>
2020-01-27 13:14:24 -0500

    modified directory structure to accomadate react in deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
-    }
-  };
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    }]
+  }
+};
