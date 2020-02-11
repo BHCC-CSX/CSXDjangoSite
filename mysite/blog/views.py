b1f5cbf8fc0fdfc3f65b55bcdfd1e4b300c95aea
ChadSutherland <csuth1@live.com>
2020-02-11 15:47:13 -0500

    Resolves #4
    Modified filter query for category view from categories__name__contains to categories__name__iexact to avoid partial matches.
    
    Added if statement to return a bad request when there are no matching categories.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,43 +2,54 @@ from django.shortcuts import render
 from blog.models import Post
 from blog.models import Comment
 from .forms import CommentForm
+from django.http import HttpResponseBadRequest
+
 # Create your views here.
+
+
 def blog_index(request):
-	posts = Post.objects.all().order_by('-created_on')
-	context = {
-		"posts":posts,
-	}
-	return render(request, "blog_index.html", context)
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
 
 def blog_category(request, category):
-	posts = Post.objects.filter(
-		categories__name__contains=category
-	).order_by('-created_on')
-	context = {
-		"category": category,
-		"posts": posts
-	}
-	return render(request, "blog_category.html", context)
+    posts = Post.objects.filter(
+        categories__name__iexact=category
+    ).order_by('-created_on')
+
+    context = {
+        "category": category,
+        "posts": posts
+    }
+
+    if len(posts.values()) == 0:
+        return HttpResponseBadRequest()
+    else:
+        return render(request, "blog_category.html", context)
+
 
 def blog_detail(request, pk):
-	post = Post.objects.get(pk=pk)
+    post = Post.objects.get(pk=pk)
 
-	form = CommentForm()
-	if request.method == 'POST':
-		form = CommentForm(request.POST)
-		if form.is_valid():
-			comment = Comment(
-				author=form.cleaned_data["author"],
-				body=form.cleaned_data["body"],
-				post=post
-			)
-			comment.save()
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
 
-	comments = Comment.objects.filter(post=post)
-	context = {
-		"post": post,
-		"comments": comments,
-		"form": form,
-	}
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+    }
 
-	return render(request, "blog_detail.html", context)
+    return render(request, "blog_detail.html", context)
